7e5ac9339c9e87ed9f7afe5f43f0c5c5cb980e29
fernando navajas <fnavajas@buk.cl>
2021-11-28 11:31:18 -0300

    feat: agregar lavadoras y secadoras

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
+from microwaves.find_microwaves import find_microwaves
 from notebooks.find_notebooks import find_notebooks
 from applewatchs.find_applewatchs import find_applewatchs
 from refrigerators.find_refrigerators import find_refrigerators
 from stereos.find_stereos import find_stereos
 from televisions.find_televisions import find_televisions
+from washerdryer.find_washerdryers import find_washerdryers
 
 
 # Cargar las opcines del web driver
@@ -15,11 +17,13 @@ chrome_options.add_argument("--headless")
 s = Service('/bin/chromedriver')
 driver = webdriver.Chrome(service= s, options=chrome_options)
 
-# find_notebooks(driver)
-# find_applewatchs(driver)
-# find_refrigerators(driver)
-# find_televisions(driver)
+find_notebooks(driver)
+find_applewatchs(driver)
+find_refrigerators(driver)
+find_televisions(driver)
 find_stereos(driver)
+find_washerdryers(driver)
+find_microwaves(driver)
 
 # Cerrar el navegador falso
 driver.quit()
---
+++ washerdryer/find_washerdryers.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+
+from order_date_prices import order_date_prices
+
+def find_washerdryers(driver):
+  washerdryers = []
+  for index in range(1, 99):
+    # Obtener los washerdryeres
+    driver.get('https://www.solotodo.cl/washing_machines?w_types=362007&ordering=offer_price_usd&page='+str(index)+'&')
+    washerdryers_elements = driver.find_elements_by_xpath('//*[@id="category-browse-results-card"]/div/div[2]/div/div[@class = "d-flex flex-column category-browse-result"]')
+    if not washerdryers_elements:
+      break
+    for washerdryer_element in washerdryers_elements:
+      washerdryer = {
+        'id': int(washerdryer_element.find_element(By.XPATH,'.//h3/a').get_attribute('href').split("-")[0].split("/")[-1]),
+        'name': washerdryer_element.find_element(By.XPATH,'.//h3/a').text,
+        'type': washerdryer_element.find_element(By.XPATH,'.//div[2]/dl/dd[1]').text,
+        'power': washerdryer_element.find_element(By.XPATH,'.//div[2]/dl/dd[2]').text,
+        'washing capacity': washerdryer_element.find_element(By.XPATH,'.//div[2]/dl/dd[3]').text,
+        'drying capacity': washerdryer_element.find_element(By.XPATH,'.//div[2]/dl/dd[4]').text,
+        'door position': washerdryer_element.find_element(By.XPATH,'.//div[2]/dl/dd[5]').text,
+        'url': washerdryer_element.find_element(By.XPATH,'.//h3/a').get_attribute('href'),
+        'price': int(washerdryer_element.find_element(By.XPATH,'.//div[3]/div/a').text.split(" ")[1].replace(".",""))
+      }
+      washerdryers.append(washerdryer)
+    # Cambiar la pagina
+    print("cambio la pagina " + index)
+
+  # Guardar la lista de washerdryeres
+  order_date_prices(washerdryers, path='washerdryers/washerdryers.csv', order='price')
+
