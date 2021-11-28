819bf319b56706eaf83fa6f8a99f7cf403280a14
fernando navajas <fnavajas@buk.cl>
2021-11-28 11:12:04 -0300

    feat: agregar equipos de musica

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from notebooks.find_notebooks import find_notebooks
 from applewatchs.find_applewatchs import find_applewatchs
 from refrigerators.find_refrigerators import find_refrigerators
+from stereos.find_stereos import find_stereos
 from televisions.find_televisions import find_televisions
 
 
@@ -14,10 +15,11 @@ chrome_options.add_argument("--headless")
 s = Service('/bin/chromedriver')
 driver = webdriver.Chrome(service= s, options=chrome_options)
 
-find_notebooks(driver)
-find_applewatchs(driver)
-find_refrigerators(driver)
-find_televisions(driver)
+# find_notebooks(driver)
+# find_applewatchs(driver)
+# find_refrigerators(driver)
+# find_televisions(driver)
+find_stereos(driver)
 
 # Cerrar el navegador falso
 driver.quit()
---
+++ stereos/find_stereos.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException, StaleElementReferenceException
+
+from order_date_prices import order_date_prices
+
+def find_stereos(driver):
+  stereos = []
+  for index in range(1, 99):
+    # Obtener los equipos de audio
+    driver.get('https://www.solotodo.cl/stereo_systems?brands=371408&brands=371454&brands=994212&brands=800910&brands=1141477&brands=371588&ordering=offer_price_usd&page='+str(index)+'&')
+    stereos_elements = driver.find_elements_by_xpath('//*[@id="category-browse-results-card"]/div/div[2]/div/div[@class = "d-flex flex-column category-browse-result"]')
+    if not stereos_elements:
+      break
+    for stereo_element in stereos_elements:
+      stereo = {
+        'id': int(stereo_element.find_element(By.XPATH,'.//h3/a').get_attribute('href').split("-")[0].split("/")[-1]),
+        'name': stereo_element.find_element(By.XPATH,'.//h3/a').text,
+        'type': stereo_element.find_element(By.XPATH,'.//div[2]/dl/dd[1]').text,
+        'brand': stereo_element.find_element(By.XPATH,'.//div[2]/dl/dd[2]').text,
+        'power': stereo_element.find_element(By.XPATH,'.//div[2]/dl/dd[3]').text,
+        'url': stereo_element.find_element(By.XPATH,'.//h3/a').get_attribute('href'),
+        'price': int(stereo_element.find_element(By.XPATH,'.//div[3]/div/a').text.split(" ")[1].replace(".",""))
+      }
+      stereos.append(stereo)
+    # Cambiar la pagina
+    print("cambio la pagina")
+
+  # Guardar la lista de equipos de audio
+  order_date_prices(stereos, path='stereos/stereos.csv', order='price')
+
